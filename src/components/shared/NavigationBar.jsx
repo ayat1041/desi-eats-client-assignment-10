74af1f0be2ec96a8ce717788a4dbf86c214d2406
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-05-04 01:31:53 +0600

    navbar added

--- src/components/shared/NavigationBar.jsx
+++ src/components/shared/NavigationBar.jsx
@@ -1,15 +1,79 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
-    return (
-        <nav>
-            <Link to="/">Home</Link>
+  return (
+    <nav>
+      {/* <Link className='text-4xl' to="/">Home</Link>
             <Link to="/login">Login</Link>
             <Link to="/signin">Sign up</Link>
-            <Link to="/blogs">Blogs</Link>
-        </nav>
-    );
+            <Link to="/blogs">Blogs</Link> */}
+      <div className="bg-orange-600 navbar shadow-md">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden px-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signin">Sign up</Link>
+              </li>
+              <li>
+                <Link to="/blogs">Blogs</Link>
+              </li>
+            </ul>
+          </div>
+          <p>
+            <span className="flex flex-col lg:flex-row ms-2 text-base lg:text-lg items-center font-medium lg:font-extrabold drop-shadow-lg">
+              <span className="text-yellow-400 font-bold">Desi</span>
+              <span>_eats.</span>
+            </span>
+          </p>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signin">Sign up</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blogs</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <img className="rounded-full w-10 border-2 border-yellow-200" src="https://images.ctfassets.net/lh3zuq09vnm2/yBDals8aU8RWtb0xLnPkI/19b391bda8f43e16e64d40b55561e5cd/How_tracking_user_behavior_on_your_website_can_improve_customer_experience.png" alt="" />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavigationBar;
