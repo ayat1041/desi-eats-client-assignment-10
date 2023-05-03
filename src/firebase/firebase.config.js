52072988c068a60865f2a28e70da614aeb493393
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-05-04 00:31:42 +0600

    done setting up firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWGx4gAyyqFimRqQDas3kE7cfywg3FWos",
+  authDomain: "desi-eats-d9f96.firebaseapp.com",
+  projectId: "desi-eats-d9f96",
+  storageBucket: "desi-eats-d9f96.appspot.com",
+  messagingSenderId: "602968379810",
+  appId: "1:602968379810:web:dc27f178383bb046f1a2fb"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
