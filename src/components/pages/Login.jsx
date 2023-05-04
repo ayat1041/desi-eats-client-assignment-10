46547e415793d9db7296f8f056cf572521563b57
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-05-04 15:53:55 +0600

    login page created

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
-
+import React from "react";
+import { FcGoogle } from 'react-icons/fc'
+import { VscGithub } from 'react-icons/vsc'
 const Login = () => {
-    return (
-        <div>
-            <h2>login</h2>
+  return (
+    <div>
+      <div className="relative flex flex-col justify-center h-screen overflow-hidden">
+        <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-lg">
+          <h1 className="text-3xl font-semibold text-center text-orange-600">
+            Login
+          </h1>
+          <form className="space-y-4">
+            <div>
+              <label className="label">
+                <span className="text-base label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Email Address"
+                className="w-full input input-bordered input-primary border-red-300"
+              />
+            </div>
+            <div>
+              <label className="label">
+                <span className="text-base label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Enter Password"
+                className="w-full input input-bordered input-primary border-red-300 mb-4"
+              />
+            </div>
+            <a
+              href="#"
+              className="text-gray-600 hover:underline hover:text-blue-600"
+            >
+              Forget Password?
+            </a>
+            <div>
+              <button className="btn btn-warning w-full m-1">Login</button>
+            </div>
+          </form>
+          
+          <div>
+              <button className="btn bg-sky-100 text-slate-950 border-0 w-full m-1">Login using Google<FcGoogle className="ml-4 text-xl"/></button>
+            </div>
+            
+            <div>
+              <button className="btn bg-sky-100 text-slate-950 border-0 w-full m-1">Login using Github<VscGithub className="ml-4 text-xl"/></button>
+            </div>
         </div>
-    );
+
+      </div>
+    </div>
+  );
 };
 
 export default Login;
