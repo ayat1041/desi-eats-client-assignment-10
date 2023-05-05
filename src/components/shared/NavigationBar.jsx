3f53bb7e41885a4aac9cb05eb0e9f5907b9a8b7e
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-05-05 15:06:31 +0600

    hamburger menu bg changed

--- src/components/shared/NavigationBar.jsx
+++ src/components/shared/NavigationBar.jsx
@@ -39,7 +39,7 @@ const NavigationBar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-slate-900 rounded-box w-52"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-orange-500 rounded-box w-52"
             >
               <li>
                 <ActiveLink to="/" title="Home"></ActiveLink>
